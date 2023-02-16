55f70f7abbe335230dbc87ed3362f9c2fc7dd090
Ajay Jain <113359960+A00455849@users.noreply.github.com>
2023-02-15 20:06:31 -0400

    main stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+from sklearn import tree
+from joblib import dump, load
+
 
 st.title("Deploying the model")
+LABELS = ['setosa', 'versicolor', 'virginica']
 
+classifier = load("IrisClassifier.joblib")
+
+#%%
+
+sp_l = st.slider('sepal length (cm)',min_value=0, max_value=10)
+sp_w = st.slider('sepal width (cm)',min_value=0, max_value=10)
+pt_l = st.slider('petal length (cm)',min_value=0, max_value=10)
+pt_w = st.slider('petal length (cm)',min_value=0, max_value=10)
+
+prediction = classifier.predict(sp_l,sp_w,pt_l,pt_w)
+
+st.write(LABELS[prediction[0]])
